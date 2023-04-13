d29612b84005eeb327d78bbfd0ea7eed10329fdf
Tomáš Kafka <tk@tomaskafka.com>
2023-04-13 11:58:24 +0200

    Initial Commit

---
+++ testColorSchemeWhenBackgrounding/testColorSchemeWhenBackgroundingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testColorSchemeWhenBackgroundingApp.swift
+//  testColorSchemeWhenBackgrounding
+//
+//  Created by Tomas Kafka on 13.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct testColorSchemeWhenBackgroundingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
